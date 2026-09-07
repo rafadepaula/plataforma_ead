24a0cd5eec8a6bc248dd140874adf1074a982327
Rafael de Paula <heeyrafa@gmail.com>
2026-09-07 05:38:41 -0300

    feat(ui): add moon and sun lucide icon paths

--- resources/views/components/ui/icon.blade.php
+++ resources/views/components/ui/icon.blade.php
@@ -221,6 +221,22 @@
             <line x1="4" x2="20" y1="18" y2="18" />
             @break
 
+        @case('moon')
+            <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+            @break
+
+        @case('sun')
+            <circle cx="12" cy="12" r="4" />
+            <path d="M12 2v2" />
+            <path d="M12 20v2" />
+            <path d="m4.93 4.93 1.41 1.41" />
+            <path d="m17.66 17.66 1.41 1.41" />
+            <path d="M2 12h2" />
+            <path d="M20 12h2" />
+            <path d="m6.34 17.66-1.41 1.41" />
+            <path d="m19.07 4.93-1.41 1.41" />
+            @break
+
         @default
             <circle cx="12" cy="12" r="10" />
             <path d="M12 16v-4" />
